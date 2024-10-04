683c40c0778c54d13c7ef3e64555815673bcd359
Stepchik1 <157306287+Stepchik1@users.noreply.github.com>
2024-10-04 21:19:26 +0700

    Add files via upload

---
+++ scripts/burger.js
@@ -0,0 +1,11 @@
+    // Крестик при нажатии меню на телефоне
+    const burger = document.getElementById('burger1');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active1');
+    })
+    //
+
+    //Реализация находится в файле header.js :
+    // Если клик был вне меню и вне иконки меню
+
+    
